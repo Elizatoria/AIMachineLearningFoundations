f4ea74216c9f90c131d830c4d365069ba23dc984
Victoria Forrest <Elizatoria@gmail.com>
2024-07-14 01:49:43 -0400

    Class 10

--- Exercises/class10_langchain_joke.py
+++ Exercises/class10_langchain_joke.py
@@ -7,6 +7,9 @@ load_dotenv('C:/Users/Eliza/Documents/MachineLearning/AI_API/class7_api.env')
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 # print(openai_api_key)
 
+# Create an instance of the ChatOpenAI class
 lim = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo")
+# Use the instance to send a message to the model and get a response
 response = lim.invoke("Tell me a Joke")
+# Print the response content
 print(response.content)
